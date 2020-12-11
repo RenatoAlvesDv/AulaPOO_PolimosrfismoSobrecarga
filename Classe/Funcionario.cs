bf62200a9fd83894795d11ff0f5fcf372c8484c0
Your Name <renato_inovacao@hotmail.com>
2020-12-11 08:48:20 -0300

    Finalizado

---
+++ Classe/Funcionario.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace AulaPOO_PolimosrfismoSobrecarga.Classe
+{
+    public class Funcionario
+    {
+        public string[] lista = {"Paulo", "Tsuka", "Priscila", "Fernanda"};
+
+        public void Mostrar(){
+            foreach ( var item in lista)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(item);
+                Console.ResetColor();
+            }     
+        }
+
+        public void Mostrar(int indice){
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            if(indice < lista.Length){
+             Console.WriteLine(lista[indice]);
+            }
+            Console.ResetColor();
+        }
+
+        public void Mostrar(string busca){
+            
+            foreach (var item in lista)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if(item == busca){
+                    Console.WriteLine("Resultado da busca :" + item);
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AulaPOO_PolimosrfismoSobrecarga.Classe;
+
+namespace AulaPOO_PolimosrfismoSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciamos nossa classe Funcionario
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar ("Tsuka");
+            fun.Mostrar(7);
+
+            Console.Beep();
+        }
+
+    }
+}
